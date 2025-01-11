dc33732d12d510f35b90149465ba368e3ce1f4a6
NachitoSMO <nintendonachito@gmail.com>
2025-01-11 00:31:31 -0300

    add raw meat

--- src/plantain.lua
+++ src/plantain.lua
@@ -239,3 +239,54 @@ SMODS.Joker {
     end
   end
 }
+
+--TODO: make the 'Ante Down' popup less weird and description ig
+SMODS.Joker {
+  key = 'raw_meat',
+  loc_txt = {
+    name = 'Raw Meat',
+    text = {
+      "After beating the Boss Blind, -1 Ante and destroy all Raw Meat Jokers"
+    }
+  },
+  rarity = 3,
+  atlas = 'plantain',
+  blueprint_compat = false,
+  config = { extra = { minus_ante = -1, reduce_ante = true } },
+  loc_vars = function(self, info_queue, card)
+    return { vars = { minus_ante, reduce_ante } }
+  end,
+  pos = { x = 0, y = 0 },
+  cost = 6,
+  calculate = function(self, card, context)
+    if context.end_of_round and G.GAME.blind.boss and not context.repetition and not context.individual then
+        G.E_MANAGER:add_event(Event({
+          func = function()
+            play_sound('tarot1')
+            card.T.r = -0.2
+            card:juice_up(0.3, 0.4)
+            card.states.drag.is = true
+            card.children.center.pinch.x = true
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                  local other_meat = nil
+                  for i=1, #G.jokers.cards do
+                    other_meat = G.jokers.cards[i]
+                    if other_meat.ability.name == card.ability.name and other_meat ~= card and card.ability.extra.reduce_ante then
+                      other_meat.ability.extra.reduce_ante = false
+                    end
+                  end
+                  if card.ability.extra.reduce_ante then
+                    ease_ante(card.ability.extra.minus_ante)
+                    card_eval_status_text(card, 'jokers', nil, nil, nil, {message = 'Ante Down', colour = G.C.BLACK})
+                  end
+                  G.jokers:remove_card(card)
+                  card:remove()
+                  card = nil
+                return true; end})) 
+            return true
+          end
+      }))
+    end
+  end
+}
