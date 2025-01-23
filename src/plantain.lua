13e0a874ece1fcf5e00030740abec2886ea9c321
IcebergLettuce <iceberglettuceyt@gmail.com>
2025-01-22 23:02:37 -0500

    plantain now has plantain

--- src/plantain.lua
+++ src/plantain.lua
@@ -97,13 +97,77 @@ SMODS.current_mod.config_tab = function() --Config tab
     },
     nodes = {
       create_toggle({
-          label = "Inkblot can copy other Mod Jokers (EXPERIMENTAL)",
+          label = "does NOTHING",
           ref_table = PlConfig,
           ref_value = "wave2",
       })
     },
   }
 end
+
+SMODS.Joker {
+  key = 'plantain',
+  loc_txt = {
+    name = 'Plantain',
+    text = {
+      'Gains {C:chips}+#1#{} Chips at end',
+      'of round, {C:green}#2# in #3#{} chance',
+      'this card is destroyed',
+      '{C:inactive}(Currently {C:chips}+#4#{C:inactive} Chips)'
+    }
+  },
+
+  config = { extra = { chips_mod = 25, chance = 6} },
+  rarity = 1,
+  atlas = 'plantain',
+  blueprint_compat = true,
+  pos = { x = 0, y = 0 },
+  cost = 6,
+
+  loc_vars = function(self, info_queue, card)
+    return { vars = { card.ability.extra.chips_mod, (G.GAME.probabilities.normal or 1), card.ability.extra.chance, (G.GAME.pl_plantain_chips or 0) } }
+  end,
+
+  calculate = function(self, card, context)
+    if context.end_of_round and not context.blueprint and not context.repetition and not context.individual then
+      if pseudorandom('plantain') < G.GAME.probabilities.normal/card.ability.extra.chance then 
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                play_sound('tarot1')
+                card.T.r = -0.2
+                card:juice_up(0.3, 0.4)
+                card.states.drag.is = true
+                card.children.center.pinch.x = true
+                G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+                    func = function()
+                            G.jokers:remove_card(self)
+                            card:remove()
+                            card = nil
+                        return true; end})) 
+                return true
+            end
+        })) 
+        return {
+            message = 'Cooked!'
+        }
+      else
+        G.GAME.pl_plantain_chips = (G.GAME.pl_plantain_chips or 0) + card.ability.extra.chips_mod
+        return {
+            message = localize('k_upgrade_ex')
+        }
+      end
+    end
+    if context.joker_main and context.cardarea == G.jokers then
+      if G.GAME.pl_plantain_chips ~= nil then
+        return 
+        {
+          chip_mod = G.GAME.pl_plantain_chips,
+          message = localize { type = 'variable', key = 'a_chips', vars = { G.GAME.pl_plantain_chips } }
+        }
+      end
+    end
+  end
+}
 
 SMODS.Joker {
   key = 'postcard',
@@ -120,21 +184,21 @@ SMODS.Joker {
   atlas = 'plantain',
   blueprint_compat = true,
   eternal_compat = false,
-  pos = { x = 0, y = 0 },
+  pos = { x = 1, y = 0 },
   cost = 4,
   config = { extra = { Xmult = 1 } },
   loc_vars = function(self, info_queue, card)
-    return { vars = { card.ability.extra.Xmult + (G.GAME.plantain_postcards_sold or 0) } }
+    return { vars = { card.ability.extra.Xmult + (G.GAME.pl_postcards_sold or 0) } }
   end,
   calculate = function(self, card, context)
     if context.selling_self then
-      G.GAME.plantain_postcards_sold = (G.GAME.plantain_postcards_sold or 0) + 1
+      G.GAME.pl_postcards_sold = (G.GAME.pl_postcards_sold or 0) + 1
     end
     if context.joker_main and context.cardarea == G.jokers then
-      if G.GAME.plantain_postcards_sold ~= nil then
+      if G.GAME.pl_postcards_sold ~= nil then
         return {
-          Xmult_mod = card.ability.extra.Xmult + G.GAME.plantain_postcards_sold,
-          message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult + G.GAME.plantain_postcards_sold } }
+          Xmult_mod = card.ability.extra.Xmult + G.GAME.pl_postcards_sold,
+          message = localize { type = 'variable', key = 'a_xmult', vars = { card.ability.extra.Xmult + G.GAME.pl_postcards_sold } }
         }
       end
     end
@@ -154,7 +218,7 @@ SMODS.Joker {
   rarity = 1,
   atlas = 'plantain',
   blueprint_compat = true,
-  pos = { x = 1, y = 0 },
+  pos = { x = 2, y = 0 },
   cost = 4,
   calculate = function(self, card, context)
     if context.cardarea == G.play and context.repetition then
@@ -184,7 +248,7 @@ SMODS.Joker {
   rarity = 1,
   atlas = 'plantain',
   blueprint_compat = true,
-  pos = { x = 2, y = 0 },
+  pos = { x = 3, y = 0 },
   cost = 4,
   config = { extra = { mult = 7, chips = 25, bingo1 = 3, bingo2 = 7 } },
   loc_vars = function(self, info_queue, card)
@@ -203,9 +267,7 @@ SMODS.Joker {
 	end,
   calculate = function(self, card, context)
     if context.individual and context.cardarea == G.play then
-      -- :get_id tests for the rank of the card. Other than 2-10, Jack is 11, Queen is 12, King is 13, and Ace is 14.
       if context.other_card:get_id() == card.ability.extra.bingo1 or context.other_card:get_id() == card.ability.extra.bingo2 then
-        -- Specifically returning to context.other_card is fine with multiple values in a single return value, chips/mult are different from chip_mod and mult_mod, and automatically come with a message which plays in order of return.
         return {
           chips = card.ability.extra.chips,
           mult = card.ability.extra.mult,
@@ -215,240 +277,6 @@ SMODS.Joker {
     end
     if context.end_of_round and not context.repetition and not context.individual then
       card:set_ability(self, card, nil, nil)
-    end
-  end
-}
-
---portal to hell
-SMODS.Joker {
-  key = 'inkblot_joker',
-  loc_txt = {
-    name = 'Inkblot',
-    text = {
-      "Mimics a random {C:attention}Joker",
-      "every round",
-      "{C:inactive}Currently #1#"
-    }
-  },
-  rarity = 1,
-  atlas = 'plantain',
-  pos = { x = 3, y = 0 },
-  cost = 3,
-  eternal_compat = false,
-  perishable_compat = false,
-  set_ability = function(self, card, initial, delay_sprites)
-    if card.plan_set_ability_2 and not card.from_context then
-      if not card.ability.extra then
-        card.ability.extra = card.ability.plan_extra
-      end
-      card.plan_set_ability_2(self, card, initial, delay_sprites)
-    elseif G.jokers and not G.SETTINGS.paused then
-      local function deepcopy(tbl)
-        local copy = {}
-        for k, v in pairs(tbl) do
-          if type(v) == "table" then
-            copy[k] = deepcopy(v)
-          else
-            copy[k] = v
-          end
-        end
-        return copy
-      end
-      
-      local options = {}
-
-      for k, v in pairs(G.P_CENTERS) do
-        if v.key ~= 'j_Plantain_inkblot_joker' and v.set == 'Joker' and v.unlocked and v.name ~= 'Shortcut' and v.name ~= 'Four Fingers'
-        and (not v.mod or (v.mod and v.mod.id == 'plantain') or PlConfig.wave2) then
-          options[k] = v
-        end
-      end
-
-      local chosen_key = pseudorandom_element(options, pseudoseed('inkblot_joker'))
-      if chosen_key then
-
-        for k, v in pairs(card) do
-          if k == 'plan_calc_2' or k == 'plan_loc_vars_2' or k == 'plan_set_ability_2' or k == 'calc_dollar_bonus' then
-            card[k] = nil
-          end
-        end
-
-        local et = false
-        local rent = false
-        local perish = false
-        if card.ability and card.ability.eternal then
-          et = true
-        end
-        if card.ability and card.ability.perishable then
-          perish = true
-        end
-        if card.ability and card.ability.rental then
-          rent = true
-        end
-
-
-        card.added_to_deck = false
-        card:remove_from_deck()
-        card.added_to_deck = true
-        
-        local car = SMODS.create_card({set = 'Joker', key = chosen_key.key, no_edition = true})
-
-        card.ability = nil
-        card.ability = deepcopy(car.ability)
-
-        if car.ability.extra and type(car.ability.extra) ~= 'table' then
-          card.ability.plan_extra = car.ability.extra
-        elseif car.ability.extra then
-          card.ability.plan_extra = deepcopy(car.ability.extra)
-        end
-
-        card.ability.mim_key = chosen_key.key
-        G.jokers:remove_card(car)
-        car:remove()
-        car = nil
-
-        if G.P_CENTERS[chosen_key.key].calculate then
-          card.plan_calc_2 = deepcopy(G.P_CENTERS[chosen_key.key]).calculate
-        end
-
-        if G.P_CENTERS[chosen_key.key].loc_vars then
-          card.plan_loc_vars_2 = deepcopy(G.P_CENTERS[chosen_key.key]).loc_vars
-        end
-
-        if G.P_CENTERS[chosen_key.key].set_ability then
-          card.plan_set_ability_2 = deepcopy(G.P_CENTERS[chosen_key.key]).set_ability
-        end
-
-        if G.P_CENTERS[chosen_key.key].calc_dollar_bonus then
-          card.calc_dollar_bonus = deepcopy(G.P_CENTERS[chosen_key.key]).calc_dollar_bonus
-        end
-
-        local function value_exists(tbl, value)
-          for _, v in pairs(tbl) do
-              if v == value then
-                  return true
-              end
-          end
-          return false
-        end
-      
-        for k, v in pairs(deepcopy(G.P_CENTERS[chosen_key.key])) do
-          if not value_exists(card, v) then
-            table.insert(card, v)
-          end
-        end
-
-        if card.ability.name == "Invisible Joker" then 
-          card.ability.invis_rounds = 0
-        end
-        if card.ability.name == 'To Do List' then
-          local _poker_hands = {}
-          for k, v in pairs(G.GAME.hands) do
-              if v.visible then _poker_hands[#_poker_hands+1] = k end
-          end
-          local old_hand = card.ability.to_do_poker_hand
-          card.ability.to_do_poker_hand = nil
-  
-          while not card.ability.to_do_poker_hand do
-            card.ability.to_do_poker_hand = pseudorandom_element(_poker_hands, pseudoseed((card.area and card.area.config.type == 'title') and 'false_to_do' or 'to_do'))
-              if card.ability.to_do_poker_hand == old_hand then card.ability.to_do_poker_hand = nil end
-          end
-        end
-        if card.ability.name == 'Caino' then 
-          card.ability.caino_xmult = 1
-        end
-        if card.ability.name == 'Yorick' then 
-          card.ability.yorick_discards = card.ability.extra.discards
-        end
-        if card.ability.name == 'Loyalty Card' then 
-          card.ability.burnt_hand = 0
-          card.ability.loyalty_remaining = card.ability.extra.every
-        end
-  
-        card.base_cost = card.config.center.cost or 1
-  
-        card.ability.hands_played_at_create = G.GAME and G.GAME.hands_played or 0
-
-        if et then
-          card:set_eternal(true)
-        end
-
-        if rent then
-          card:set_rental(true)
-        end
-
-        if perish then
-          card:set_perishable(true)
-        end
-
-
-      end
-    end
-	end,
-  load = function(self, card, card_table, other_card)
-    local function deepcopy(tbl)
-      local copy = {}
-      for k, v in pairs(tbl) do
-        if type(v) == "table" then
-          copy[k] = deepcopy(v)
-        else
-          copy[k] = v
-        end
-      end
-      return copy
-    end
-
-    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-    func = function()
-      if card.ability.mim_key then
-      if G.P_CENTERS[card.ability.mim_key].calculate then
-        card.plan_calc_2 = deepcopy(G.P_CENTERS[card.ability.mim_key]).calculate
-      end
-
-      if G.P_CENTERS[card.ability.mim_key].loc_vars then
-        card.plan_loc_vars_2 = deepcopy(G.P_CENTERS[card.ability.mim_key]).loc_vars
-      end
-
-      if G.P_CENTERS[card.ability.mim_key].set_ability then
-        card.plan_set_ability_2 = deepcopy(G.P_CENTERS[card.ability.mim_key]).set_ability
-      end
-
-      if G.P_CENTERS[card.ability.mim_key].calc_dollar_bonus then
-        card.calc_dollar_bonus = deepcopy(G.P_CENTERS[card.ability.mim_key]).calc_dollar_bonus
-      end
-        card.ability.extra = card.ability.plan_extra
-    end
-    return true end}))
-  end,
-  loc_vars = function(self, info_queue, card)
-    if card.ability.mim_key then
-      if card.config.center.mod and card.plan_loc_vars_2 and type(card.plan_loc_vars_2) == 'function' and type(card.ability.extra) == 'table' then
-        local check = card:plan_loc_vars_2(info_queue, card)
-        if check and check.vars then
-          specific = check.vars
-        end
-        info_queue[#info_queue+1] = {type = 'descriptions', set = 'Joker', key = card.ability.mim_key, specific_vars = specific or {}}
-      else
-        info_queue[#info_queue+1] = {type = 'descriptions', set = 'Joker', key = card.ability.mim_key, specific_vars = card.plantain_info or {}}
-      end
-      return { vars = {localize{type = 'name_text', set = 'Joker', key = card.ability.mim_key}} }
-    else
-      return { vars = {"none"}}
-    end
-  end,
-  calculate = function(self, card, context)
-    if context.pl_cash_out and not card.getting_sliced and not context.repetition and not context.individual and not context.blueprint then
-      card.from_context = true
-      card:set_ability(self, card, nil, nil)
-      if card.plan_set_ability_2 then
-        card.plan_set_ability_2(self, card, nil, nil)
-      end
-      card.from_context = false
-      return card_eval_status_text(card, 'jokers', nil, nil, nil, {message = 'Updated!', colour = G.C.MONEY})
-    end
-    if card.plan_calc_2 then
-      local mim_calc = card.plan_calc_2(self, card, context)
-      return mim_calc
     end
   end
 }
