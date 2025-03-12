772a8432a0b7c4a5bb80bc70190987534c871513
IcebergLettuce <93489438+IcebergLettuce0@users.noreply.github.com>
2025-03-12 01:50:11 -0400

    cleanup + smods load_file

--- src/pl_main.lua
+++ src/pl_main.lua
@@ -15,74 +15,74 @@ SMODS.Atlas{
 SMODS.current_mod.extra_tabs = function()
   local scale = 0.5
   return {
-      label = "Credits",
-      tab_definition_function = function()
-      return {
-          n = G.UIT.ROOT,
+    label = "Credits",
+    tab_definition_function = function()
+    return {
+      n = G.UIT.ROOT,
+      config = {
+      align = "cm",
+      padding = 0.05,
+      colour = G.C.CLEAR,
+      },
+      nodes = {
+      {
+        n = G.UIT.R,
+        config = {
+        padding = 0,
+        align = "cm"
+        },
+        nodes = {
+        {
+          n = G.UIT.T,
           config = {
-          align = "cm",
-          padding = 0.05,
-          colour = G.C.CLEAR,
-          },
-          nodes = {
-          {
-              n = G.UIT.R,
-              config = {
-              padding = 0,
-              align = "cm"
-              },
-              nodes = {
-              {
-                  n = G.UIT.T,
-                  config = {
-                  text = "Programming: IcebergLettuce, NachitoSMO",
-                  shadow = false,
-                  scale = scale,
-                  colour = G.C.GREEN
-                  }
-              }
-              }
-          },
-          {
-              n = G.UIT.R,
-              config = {
-              padding = 0,
-              align = "cm"
-              },
-              nodes = {
-              {
-                  n = G.UIT.T,
-                  config = {
-                  text = "Art: IcebergLettuce",
-                  shadow = false,
-                  scale = scale,
-                  colour = G.C.PURPLE
-                  }
-              },
-              }
-          },
-          {
-              n = G.UIT.R,
-              config = {
-                  padding = 0,
-                  align = "cm"
-              },
-              nodes = {
-                  {
-                  n = G.UIT.T,
-                  config = {
-                      text = "Idea Guys: AtomicLight, BurntFrenchToast, TomatoIcecream",
-                      shadow = false,
-                      scale = scale,
-                      colour = G.C.MONEY
-                  }
-                  },
-              }
+          text = "Programming: IcebergLettuce, NachitoSMO",
+          shadow = false,
+          scale = scale,
+          colour = G.C.GREEN
           }
+        }
+        }
+      },
+      {
+        n = G.UIT.R,
+        config = {
+        padding = 0,
+        align = "cm"
+        },
+        nodes = {
+        {
+          n = G.UIT.T,
+          config = {
+          text = "Art: IcebergLettuce",
+          shadow = false,
+          scale = scale,
+          colour = G.C.PURPLE
           }
+        },
+        }
+      },
+      {
+        n = G.UIT.R,
+        config = {
+          padding = 0,
+          align = "cm"
+        },
+        nodes = {
+          {
+          n = G.UIT.T,
+          config = {
+            text = "Idea Guys: AtomicLight, BurntFrenchToast, TomatoIcecream",
+            shadow = false,
+            scale = scale,
+            colour = G.C.MONEY
+          }
+          },
+        }
       }
-      end
+      }
+    }
+    end
   }
 end
 
-NFS.load(SMODS.current_mod.path .. 'src/jokers/pl_jokers_w1.lua')()
+SMODS.load_file('src/additions/pl_jokers_w1.lua')()
