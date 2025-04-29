fa2645510387e5e582a828918ba0906ef40e5394
NachitoSMO <nintendonachito@gmail.com>
2025-04-29 16:32:23 -0300

    rebirth fix surely

--- src/additions/pl_consumables.lua
+++ src/additions/pl_consumables.lua
@@ -84,9 +84,20 @@ SMODS.Consumable{
 	end,
 
 	use = function(self, card)
-		local destroyed_card = nil
+
+
+		local rightmost = G.hand.highlighted[3]
+		local middle = G.hand.highlighted[2]
+		local leftmost = G.hand.highlighted[1]
+
 		for i=1, #G.hand.highlighted do
-			if (i ~= 2) then
+			if G.hand.highlighted[i].T.x < math.max(rightmost.T.x, middle.T.x, leftmost.T.x) and G.hand.highlighted[i].T.x > math.min(rightmost.T.x, middle.T.x, leftmost.T.x) then
+				middle = G.hand.highlighted[i]
+			end
+		end
+
+		for i=1, #G.hand.highlighted do
+			if (G.hand.highlighted[i] ~= middle) then
 				local percent = 0.85 + (i-0.999)/(#G.hand.highlighted-0.998)*0.3
            		G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.5, func = function() 
 					G.hand.highlighted[i]:flip();
@@ -94,18 +105,18 @@ SMODS.Consumable{
 					G.hand.highlighted[i]:juice_up(0.3, 0.3);
 				return true end }))
 			else
-				destroyed_card = G.hand.highlighted[i]
+				destroyed_card = middle
 			end
 		end
 		for i=1, #G.hand.highlighted do
-			if (i ~= 2) then
+			if (G.hand.highlighted[i] ~= middle) then
 				
 				G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.5,func = function()
 					local suit_prefix = string.sub(G.hand.highlighted[i].base.suit, 1, 1)..'_'
-                	local rank_suffix = G.hand.highlighted[2].base.id < 10 and tostring(G.hand.highlighted[2].base.id) or
-										G.hand.highlighted[2].base.id == 10 and 'T' or G.hand.highlighted[2].base.id == 11 and 'J' or
-										G.hand.highlighted[2].base.id == 12 and 'Q' or G.hand.highlighted[2].base.id == 13 and 'K' or
-										G.hand.highlighted[2].base.id == 14 and 'A'
+                	local rank_suffix = middle.base.id < 10 and tostring(middle.base.id) or
+					middle.base.id == 10 and 'T' or middle.base.id == 11 and 'J' or
+					middle.base.id == 12 and 'Q' or middle.base.id == 13 and 'K' or
+					middle.base.id == 14 and 'A'
 					G.hand.highlighted[i]:set_base(G.P_CARDS[suit_prefix..rank_suffix])
 					G.hand.highlighted[i]:flip();play_sound('tarot2', percent, 0.6);
 					G.hand.highlighted[i]:juice_up(0.3, 0.3);
