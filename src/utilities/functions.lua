bc2decbb82e4093e81112e5cb79781a775594b44
NachitoSMO <nintendonachito@gmail.com>
2025-05-25 14:21:14 -0300

    better popup implementation

--- src/utilities/functions.lua
+++ src/utilities/functions.lua
@@ -8,41 +8,13 @@ end
 
 function PL_UTIL.add_booster_pack()
   if not G.shop then return end
-  local pack_watch = {
-    'p_arcana_normal_1',
-    'p_arcana_normal_2',
-    'p_arcana_normal_3',
-    'p_arcana_normal_4',
-    'p_arcana_jumbo_1',
-    'p_arcana_jumbo_2',
-    'p_arcana_mega_1',
-    'p_arcana_mega_2',
-    'p_celestial_normal_1',
-    'p_celestial_normal_2',
-    'p_celestial_normal_3',
-    'p_celestial_normal_4',
-    'p_celestial_jumbo_1',
-    'p_celestial_jumbo_2',
-    'p_celestial_mega_1',
-    'p_celestial_mega_2',
-    'p_standard_normal_1',
-    'p_standard_normal_2',
-    'p_standard_normal_3',
-    'p_standard_normal_4',
-    'p_standard_jumbo_1',
-    'p_standard_jumbo_2',
-    'p_standard_mega_1',
-    'p_standard_mega_2',
-    'p_spectral_normal_1',
-    'p_spectral_normal_2',
-    'p_spectral_jumbo_1',
-    'p_spectral_mega_1',
-    'p_buffoon_normal_1',
-    'p_buffoon_normal_2',
-    'p_buffoon_jumbo_1',
-    'p_buffoon_mega_1',
-  }
-  local pack_chosen = pack_watch[ math.random( #pack_watch ) ]
+  local pack_watch = {}
+  for k, v in pairs(G.P_CENTERS) do
+    if v.set == 'Booster' then
+      table.insert(pack_watch, k)
+    end
+  end
+  local pack_chosen = pseudorandom_element(pack_watch, pseudoseed('pl_pop_up'))
   local pack = Card(
     G.shop_booster.T.x + G.shop_booster.T.w / 2,
     G.shop_booster.T.y,
